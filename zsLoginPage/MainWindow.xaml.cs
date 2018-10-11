b5a6085bb3d81db60af377c5c056db8ce12eb16b
Zach Shaver <shaverz@uwindsor.ca>
2018-10-11 00:28:10 -0400

    Develop MainWindow for Book Orders

---
+++ zsLoginPage/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookStoreGUI
+{
+     /// <summary>
+     /// Interaction logic for MainWindow.xaml
+     /// </summary>
+     public partial class MainWindow : Window
+     {
+          public MainWindow()
+          {
+               InitializeComponent();
+          }
+
+          private void listViewOrders_SelectionChanged(object sender, SelectionChangedEventArgs e)
+          {
+
+          }
+
+          private void LoginButton_Click(object sender, RoutedEventArgs e)
+          {
+
+          }
+
+          private void ExitButton_Click(object sender, RoutedEventArgs e)
+          {
+
+          }
+
+          private void AddBookButton_Click(object sender, RoutedEventArgs e)
+          {
+
+          }
+
+          private void CheckoutButton_Click(object sender, RoutedEventArgs e)
+          {
+
+          }
+     }
+}
