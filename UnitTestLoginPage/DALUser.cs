eaa5f72847686c48aa81a878d52487c223facbca
Zach Shaver <shaverz@uwindsor.ca>
2018-10-11 05:17:23 -0400

    Extend User according to new XYDatabase

--- UnitTestLoginPage/DALUser.cs
+++ UnitTestLoginPage/DALUser.cs
@@ -12,35 +12,63 @@ namespace BookStoreLib
      */
      public class DALUser
      {
-          public int Login(string username, string password)
+          //private static string connString = Properties.Settings.Default.ZSDatabaseConnectionString;
+          private static string connString = Properties.Settings.Default.XYDatabaseConnectionString; // Use new DB provided for assignment 3
+
+          // Feels awkward to store variables redundantly like this. Aassignment 3
+          // requires the extra DAL class, but also access to variables like the full name
+          // TODO: Talk to Dr Yuan about how strict assignment is on implementation
+          public int Id { get; private set; }
+          public string Username { get; private set; }
+          public string Password { get; private set; }
+          public string FullName { get; private set; }
+          public string Type { get; private set; }
+          public bool IsManager { get; private set; }
+
+          public bool Login(string username, string password)
           {
-               var conn = new SqlConnection(Properties.Settings.Default.ZSDatabaseConnectionString);
-               try
-               {
-                    SqlCommand loginSQL = new SqlCommand
-                    {
-                         Connection = conn,
-                         CommandText = "Select Id from [User] where "
-                         + "Username = @Username and Password = @Password "
-                    };
-                    loginSQL.Parameters.AddWithValue("@Username", username);
-                    loginSQL.Parameters.AddWithValue("@Password", password);
-                    conn.Open();
-                    int id = (int)loginSQL.ExecuteScalar();
-                    return id < 0 ? -1 : id;
-               }
-               catch (Exception e)
-               {
-                    Console.WriteLine(e.ToString());
-                    return -1;
-               }
-               finally
-               {
-                    if (conn.State == System.Data.ConnectionState.Open)
-                    {
-                         conn.Close();
-                    }
-               }
+
+            var conn = new SqlConnection(connString);
+            try
+            {
+                SqlCommand loginSQL = new SqlCommand
+                {
+                    Connection = conn,
+                    CommandText = "Select * from [User] where "
+                     + "Username = @Username and Password = @Password "
+                };
+                loginSQL.Parameters.AddWithValue("@Username", username);
+                loginSQL.Parameters.AddWithValue("@Password", password);
+                conn.Open();
+                var reader = loginSQL.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    Id = (int)reader["Id"];
+                    Username = (string)reader["Username"];
+                    Password = (string)reader["Password"];
+                    FullName = (string)reader["FullName"];
+                    Type = (string)reader["Type"];
+                    IsManager = (bool)reader["Manager"];
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return false;
+            }
+            finally
+            {
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
           }
      }
 }
