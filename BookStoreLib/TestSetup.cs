ec218cafafb8763d467338e651ce18bd685120f9
Zach Shaver <shaverz@uwindsor.ca>
2018-10-30 19:03:52 -0400

    Add test initialize step to reset the database

---
+++ BookStoreLib/TestSetup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.SqlClient;
+using System.IO;
+using System.Reflection;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace BookStoreLib
+{
+    [TestClass]
+    public class TestSetup
+    {
+        [AssemblyInitialize]
+        public static void InitializeTests(TestContext context)
+        {
+            // Reset database before tests run
+            string connString = Properties.Settings.Default.DatabaseConnectionString;
+
+            string script = File.ReadAllText("db_create.sql");
+            
+            // split script on GO command
+            System.Collections.Generic.IEnumerable<string> commandStrings = Regex.Split(script, @"^\s*GO\s*$",
+                                     RegexOptions.Multiline | RegexOptions.IgnoreCase);
+
+            using (SqlConnection connection = new SqlConnection(connString))
+            {
+                connection.Open();
+                foreach (string commandString in commandStrings)
+                {
+                    if (commandString.Trim() != "")
+                    {
+                        using (var command = new SqlCommand(commandString, connection))
+                        {
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
