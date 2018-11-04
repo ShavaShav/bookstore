3dbc84ff66c21bd39e74c6544ebd388aed43d335
Akshit Tripathi <tripati@uwindsor.ca>
2018-11-04 02:27:22 -0500

    Optimized information verification process during registration.
    - Moved all regex string from various funtions into const states in the class.
    
    - Added Trim() to all function checks.
    
    - Confirm password box is verified against password.
    
    - User's data is formatted before being sent to DB.
    
    - DEBUG: Final user data is shown in a dialog before registration is performed.

--- BookStoreGUI/UserRegisterDialog.xaml.cs
+++ BookStoreGUI/UserRegisterDialog.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using BookStoreLib;
 using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +12,16 @@ namespace BookStoreGUI
     /// </summary>
     public partial class UserRegisterDialog : Window
     {
+        private const String emailPattern = @"^[\w!#$%&'*+\-/=?\^_`{|}~]+(\.[\w!#$%&'*+\-/=?\^_`{|}~]+)*"
+                                   + "@"
+                                   + @"((([\-\w]+\.)+[a-zA-Z]{2,4})|(([0-9]{1,3}\.){3}[0-9]{1,3}))$";
+
+        private const String phonePattern = @"^\(?([0-9]{3})\)?[-.● ]?([0-9]{3})[-.●]?([0-9]{4})$";
+
+        private const String postalCodePattern = @"[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ] ?[0-9][ABCEGHJKLMNPRSTVWXYZ][0-9]";
+
+        private const String numberFilter = @"[^\d]";
+
         public UserRegisterDialog()
         {
             InitializeComponent();
@@ -20,15 +31,16 @@ namespace BookStoreGUI
         {
             // Check if any of the required fields are empty
             if(
-                UsernameBox.Text.Equals("") ||
-                FirstNameBox.Text.Equals("") || 
-                LastNameBox.Text.Equals("") ||
-                EmailBox.Text.Equals("") ||
-                PhoneBox.Text.Equals("") ||
-                AddressLine1Box.Text.Equals("") ||
-                CityBox.Text.Equals("") ||
+                UsernameBox.Text.Trim().Equals("") ||
+                PasswordBox.Password.Trim().Equals("") ||
+                FirstNameBox.Text.Trim().Equals("") || 
+                LastNameBox.Text.Trim().Equals("") ||
+                EmailBox.Text.Trim().Equals("") ||
+                PhoneBox.Text.Trim().Equals("") ||
+                AddressLine1Box.Text.Trim().Equals("") ||
+                CityBox.Text.Trim().Equals("") ||
                 ProvinceComboBox.SelectedIndex == -1 ||
-                PostalCodeBox.Text.Equals("") 
+                PostalCodeBox.Text.Trim().Equals("") 
                 )
             {
                 MessageBox.Show("Please make sure to all required information has been entered.", "Incomplete Form");
@@ -36,24 +48,31 @@ namespace BookStoreGUI
             }
 
             // TODO: Check if username exists in database. Display a messagebox in that case.
-            // TODO: Check if email exists in database. Display a messagebox in that case.
+            // TODO: Check if email exists in database. Display a messagebox in that case
+
+            // Check if password matches confirm password box
+            if(PasswordBox.Password != ConfirmPasswordBox.Password)
+            {
+                MessageBox.Show("The passwords do not match. Please check the passwords you have entered", "Password Mismatch");
+                return;
+            }
 
             // Check if a valid email is entered
-            if (!IsValidEmail(EmailBox.Text))
+            if (!Regex.IsMatch(EmailBox.Text, emailPattern))
             {
                 MessageBox.Show("Invalid email address entered. Please check the email entered.", "Invalid Email");
                 return;
             }
 
             // Check if a valid phone number is entered
-            if (!IsValidPhone(PhoneBox.Text))
+            if (!Regex.IsMatch(PhoneBox.Text, phonePattern))
             {
                 MessageBox.Show("Invalid phone number entered. Please check the number entered.", "Invalid Phone number");
                 return;
             }
 
             // Check if a valid postal code is entered
-            if(!IsValidPostalCode(PostalCodeBox.Text))
+            if(!Regex.IsMatch(PostalCodeBox.Text.Trim().ToUpper(), postalCodePattern))
             {
                 MessageBox.Show(
                     "Invalid postal code entered. Please check the postal code you have entered.\n\n" +
@@ -62,48 +81,42 @@ namespace BookStoreGUI
                 return;
             }
 
-            // If no error is detected, create the User object to pass to the register function
-            User user = new User(
-                UsernameBox.Text, FirstNameBox.Text, LastNameBox.Text, 
-                EmailBox.Text, PhoneBox.Text, AddressLine1Box.Text, 
-                AddressLine2Box.Text, CityBox.Text, 
-                ProvinceComboBox.SelectedValue.ToString(), PostalCodeBox.Text);
+            // If no error is detected, prepare the fields
+            var username = UsernameBox.Text.Trim();
+            var firstName = FirstNameBox.Text.Trim();
+            firstName = firstName.First().ToString().ToUpper() + firstName.Substring(1);
+            var lastName = LastNameBox.Text.Trim();
+            lastName = lastName.First().ToString().ToUpper() + lastName.Substring(1);
+            var email = EmailBox.Text.Trim().ToLower();
+            var phone = Regex.Replace(PhoneBox.Text.Trim(), numberFilter, "");
+            var addressLine1 = AddressLine1Box.Text.Trim();
+            var addressLine2 = AddressLine2Box.Text.Trim();
+            var city = CityBox.Text.Trim();
+            var province = ProvinceComboBox.Text;
+            var postalCode = PostalCodeBox.Text.Trim().ToUpper();
+            postalCode.Replace(" ", "");
 
-            Account.Register(user, PasswordBox.Password);
+            // Create the User object to pass to the register function
+            User user = new User(
+                username, firstName, lastName, email, phone, addressLine1, 
+                addressLine2, city, province, postalCode);
+
+            // DEBUG: Show the user's info before starting registration process
+            MessageBox.Show(user.ToString(), "User Info from User.cs");
+
+            if(Account.Register(user, PasswordBox.Password) == null)
+            {
+                String errorString = "Login failed due to the following errors:\n\n";
+                Account.ErrorMessages.ForEach(error => {
+                    errorString += "● " + error +".\n";
+                });
+                MessageBox.Show(errorString, "Login Failed");
+            }
         }
 
         private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
-        }
-
-        private bool IsValidEmail(string emailaddress)
-        {
-            String theEmailPattern = @"^[\w!#$%&'*+\-/=?\^_`{|}~]+(\.[\w!#$%&'*+\-/=?\^_`{|}~]+)*"
-                                   + "@"
-                                   + @"((([\-\w]+\.)+[a-zA-Z]{2,4})|(([0-9]{1,3}\.){3}[0-9]{1,3}))$";
-
-            if (Regex.IsMatch(emailaddress, theEmailPattern))
-                return true;
-            return false;
-        }
-
-        private bool IsValidPhone(string phonenumber)
-        {
-            String phonePattern = @"^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$";
-
-            if (Regex.IsMatch(phonenumber, phonePattern))
-                return true;
-            return false;
-        }
-
-        private bool IsValidPostalCode(string postalcode)
-        {
-            String postalCodePattern = @"[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ] ?[0-9][ABCEGHJKLMNPRSTVWXYZ][0-9]";
-
-            if (Regex.IsMatch(postalcode, postalCodePattern))
-                return true;
-            return false;
         }
     }
 }
